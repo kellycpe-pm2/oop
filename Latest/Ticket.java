40d4e81ac4d1e3bdd0783102b7c0b524c9f86151
Ybbtsswl <yingjieng678@gmail.com>
2026-04-07 10:42:14 +0800

    Fix ticketId assignment in Ticket.java
    Signed-off-by: Ybbtsswl <yingjieng678@gmail.com>

--- Latest/Ticket.java
+++ Latest/Ticket.java
@@ -30,8 +30,8 @@ public class Ticket {
         this.status=status;
         this.eventId = eventId;
         this.purchaseDate = LocalDate.now();
-        this.ticketId = "T" + String.format("%05d", ticketCount);
         ticketCount++;
+        this.ticketId = "T" + String.format("%05d", ticketCount);
         this.totalAmount=tt.getPrice(ticketType);
         this.seatNo=tt.getSeat(ticketType);
         this.perks=tt.getPerks();
@@ -119,6 +119,7 @@ public class Ticket {
     // display ticket details
     public void displayTicketDetails() {
         System.out.println("Ticket");
+        System.out.println("-----------------------");
         System.out.println("Ticket ID: " + ticketId);
         System.out.println("Booking ID: " + bookingId);
         System.out.println("Event ID: " + eventId);
