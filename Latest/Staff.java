161335c1998f4e595ff06cc015d1e61ebda1a79b
yintaobing-sudo <yintaobing@gmail.com>
2026-04-11 06:08:19 +0800

    Update Staff.java
    Signed-off-by: yintaobing-sudo <yintaobing@gmail.com>

--- Latest/Staff.java
+++ Latest/Staff.java
@@ -1,6 +1,6 @@
 import java.util.List;
 public class Staff extends User{
-    private static int checkin_couter=0;
+    private static int total_Checkin_counter=0;
     private final String role= "Staff";
     public Staff(){
         super();
@@ -10,31 +10,31 @@ public class Staff extends User{
         super(username, password, email, contactNo);
     }
 
-    public static void increase_CheckIn_Couter(){
-        Staff.checkin_couter++;
+    public static void increase_total_Checkin_counter(){
+        Staff.total_Checkin_counter++;
     }
     
-    public static int getCheckin_Couter(){
-        return Staff.checkin_couter;
+    public static int gettotal_Checkin_counter(){
+        return Staff.total_Checkin_counter;
     }
 
     public String toString(){
-            return super.toString()+String.format("║          Position       :  %-31s║\n"+
-                                                  "║          Total Check-In :  %-31d║\n",role,checkin_couter);
+            return super.toString()+String.format("|          Position       :  %-31s|\n"+
+                                                  "|          Total Check-In :  %-31d|\n",role,total_Checkin_counter);
  
 
     }
     public static int checkTotal_CheckIn( List <Ticket> tickets){
         for (Ticket ticket : tickets){
-            if(!ticket.getStatus()){
-                Staff.checkin_couter++;
+           if (ticket != null && !ticket.getStatus()) {
+                Staff.total_Checkin_counter++;
             }
         }
-        return Staff.checkin_couter;
+        return Staff.total_Checkin_counter;
     }
 
-   public boolean checkClass(Object o){
-        if(o instanceof Staff){
+    public boolean checkClass(Object o) {
+        if (o instanceof Staff) {
             return true;
         }
         return false;
@@ -46,5 +46,12 @@ public class Staff extends User{
         }else{
             return false;
         }
+    }
+
+    public boolean hasUser(String username) {
+        if (getUsername().equals(username)) {
+            return true;
+        }
+        return false;
     }
 }
