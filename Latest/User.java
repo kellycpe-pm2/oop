30a649484f09240094ad322b5b12a1dd22977e83
yintaobing-sudo <yintaobing@gmail.com>
2026-04-11 06:12:01 +0800

    Update User.java
    Signed-off-by: yintaobing-sudo <yintaobing@gmail.com>

--- Latest/User.java
+++ Latest/User.java
@@ -1,345 +1,62 @@
 
 public class User {
-    // Sign Up of Instance
-    private String signUpName;
-    private String signUpEmail;
-    private String signUpContactNo;
-    private String signUpPassword;
-    private String signUpPassword2;
-    
-
-    // Login of Instance
-    private String loginUsername;
-    private String loginPassword;
 
     // the user
     private String username;
-    private String pswd;
+    private String password;
     private String email;
     private String contactNo;
-    private int [] no = new int [2];
 
     // --------------------------Sign Up of Constructor---------------------------
     // default constructor
     User() {
-        this(null,null,null,null,null);
+        this(null,null,null,null);
     }
 
     // parameterized constructor
-    User(String signUpName, String signUpEmail, String signUpPassword, String signUpPassword2, String signUpContactNo) {
-        this.signUpName = signUpName;
-        this.signUpEmail = signUpEmail;
-        this.signUpContactNo=signUpContactNo;
-        this.signUpPassword = signUpPassword;
-        this.signUpPassword2 = signUpPassword2;
-    }
-
-    User(String loginUsername, String loginPassword) {
-        this.loginPassword = loginPassword;
-        this.loginUsername = loginUsername;
-    }
-
      User(String username , String password, String email,String contactNo) {
         this.username =username;
         this.email = email;
         this.contactNo=contactNo;
-        this.pswd = password;
+        this.password = password;
     }
 
     
 
     // -------------------------- getter---------------------------
-    public String getSignUpName() {
-        return this.signUpName;
-    }
 
-    public String getSignUpEmail() {
-        return this.signUpEmail;
-    }
-
-    public String getSignUpPassword() {
-        return this.signUpPassword;
-    }    
-    public String getSignUpContactNo() {
-        return this.signUpContactNo;
-    }
-
-    public String getLoginPassword() {
-        return loginPassword;
-    }
-
-    public String getAccessUsername() {
+    public String getUsername() {
         return this.username;
     }
 
-    public String getAccessEmail() {
+    public String getEmail() {
         return this.email;
     }
-    public String getAccessContactNo() {
+    public String getContactNo() {
         return this.contactNo;
     }
 
-    public String getAccessPassword() {
-        return this.pswd;
+    public String getPassword() {
+        return this.password;
     }
-
-    public int getno() {
-        return this.no[1];
-    }
-
     // -------------------------- setter---------------------------
-    public void setSignUpName(String signUpName) {
-        this.signUpName = signUpName.strip();
-    }
-
-    public void setSignUpEmail(String signUpEmail) {
-        this.signUpEmail = signUpEmail.strip();
-    }
-
-    public void setSignUpContactNo(String signUpContactNo){
-        this.signUpContactNo=signUpContactNo.strip();
-    }
-
-    public void setSignUpPassword(String signUpPassword) {
-        this.signUpPassword = signUpPassword.strip();
-    }
-
-    public void setSignUpPassword2(String signUpPassword2) {
-        this.signUpPassword2 = signUpPassword2.strip();
-    }
-
-    public void setLoginUsername(String loginUsername) {
-        this.loginUsername = loginUsername.strip();
-    }
-
-    public void setLoginPassword(String loginPassword) {
-        this.loginPassword = loginPassword.strip();
-    }
-
-    public void setNo(int no){
-        this.no[1]=no;
-    }
     
-    public void setAccessUserName(String username){
+    public void setUserName(String username){
         this.username=username;
     }
-    // -------------------------- method---------------------------
-
-    // -------------------------Access User------------------------
-
-    public void signUpUser() {
-        username = signUpName;
-        email = signUpEmail;
-        contactNo=signUpContactNo;
-        pswd = signUpPassword;
-    }
-
-    public void loginUser( String email,String  contactNo) {
-        username = loginUsername;
-        this.email = email;
-        this.contactNo=contactNo;
-        pswd = loginPassword;
-    }
-
-    // validation for signup
-    public boolean validationExist(User [] existUser) {
-        
-        if (validationEmpty(this.signUpName)) {
-
-            return false;
-        }
-
-        if (existUser == null){
-            return false;
-        }
-            username=signUpName;
-
-            for (int i = 0; i < existUser.length; i++) {
-                if (existUser[i] != null &&this.equals(existUser[i])) {
-                    System.out.println("Error: The Username Has Already Exist ! ");
-                    username=null;
-                    return false;
-                }
-            }
-        
-
-        return true;
-    }
-
-    public boolean validationName() {
-        char[] namearray = this.signUpName.toCharArray();
-        if (validationEmpty(this.signUpName)) {
-            return false;
-
-        } else if (namearray.length < 3) {
-            System.out.println("Input Error : Your Name length must be at least 3 length.");
-            return false;
-        }
-
-        else {
-
-            for (char charname : namearray) {
-                if (!((int) charname >= 65 && (int) charname <= 90)
-                        && !((int) charname >= 97 && (int) charname <= 122)) {
-                    System.out.println("Input Error : Please Enter In alpha !");
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-    }
-
-    public boolean validationEmail() {
-        if (validationEmpty(this.signUpEmail)) {
-            return false;
-        }
-        char[] emailArray = this.signUpEmail.toCharArray();
-
-        // check the first character of email
-        if (!((int) emailArray[0] >= 65 && (int) emailArray[0] <= 90)
-                && !((int) emailArray[0] >= 97 && (int) emailArray[0] <= 122)) {
-            System.out.println("Input Error: The first Character Cannot Be Symbols ! ");
-            return false;
-        }
-        // check the email format
-        else if (!(this.signUpEmail.contains("@gmail.com"))) {
-
-            System.out.println("Input Error: Please Input In Gmail Format ! ");
-            return false;
-
-        } else {
-
-            return true;
-
-        }
-    }
    
-   //validation for contact number
-    public boolean validationContactNo(){ 
-
-        char[] contactNoArray = this.signUpContactNo.toCharArray();
-         if (validationEmpty(this.signUpContactNo)) {
-            return false;
-        }
-       
-
-        // check the contact number length 
-        else if(contactNoArray.length!=11&& contactNoArray.length!=10 ){
-            System.out.println("Input Error: Please Enter In Format ! ");
-            return false;
-
-        }
-                 // check the character is in number
-
-        else{
-            for (int i=0; i < contactNoArray.length; i++){
-            if (!((int) contactNoArray[i] >= 48 && (int) contactNoArray[i] <= 57)) {
-            System.out.println("Input Error: Please Enter In Format ! ");
-            return false;
-            }
-        }
-            
-        return true;
-
-        }
-
-        
-    }
-    // validation password (it length must be more than 5 char)
-    public boolean validationPassword() {
-        if (validationEmpty(this.signUpPassword)) {
-            return false;
-        }
-        if (this.signUpPassword.length() < 5) {
-
-            System.out.println("Input Error: Your Password Must be More Than 5 Character ! ");
-
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    public boolean validationPassword2() {
-        if (validationEmpty(this.signUpPassword2)) {
-            return false;
-        }
-        if (!(this.signUpPassword2.equals(this.signUpPassword))) {
-            System.out.println(" Error: Your Password Is Not Matched ! ");
-
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    public boolean validationEmpty(String data) {
-        if (data == null) {
-            System.out.println("Input Error: Don't Empty Your Input ! ");
-
-            return true;
-
-        } else if (data.isEmpty()) {
-            System.out.println("Input Error: Don't Empty Your Input ! ");
-
-            return true;
-
-        } else {
-
-            return false;
-        }
-    }
-
-
-
-    // validation for login
-
-    public boolean validationNoExistName(User [] existUser, int [] no) {
-        //check the user input is empty or not
-        if (validationEmpty(this.loginUsername)) {
-            return false;
-        }
-            username=loginUsername;
-            for (int j=0; j< no[0] ; j++) {
-                
-                if (existUser[j]!=null&& equals(existUser[j])) {
-                this.no[0] =j;
-                this.no[1]=j;
-                //found user
-                return true;
-            }
-        }
-        username=null;
-        System.out.println("Error: The Username Is Not Matched ! ");
-
-        return false;
-       
-    }
-
-    public boolean validationLoginPwd(User [] user) {
-        if (validationEmpty(this.loginPassword)) {
-            return false;
-        }
-
-
-        String current_pswd= user[no[0]].getAccessPassword();
-        if (this.loginPassword.equals(current_pswd)) {
-            this.pswd=loginPassword;
-            return true;
-
-        }
-        System.out.println("Error: Inccorrect Password ! ");
-
-        return false;
+   
+    // -------------------------- method---------------------------
+    public boolean validateUser() {
+        return username != null && password != null && 
+               email != null && contactNo != null;
     }
 
     public String toString(){
         return String.format(
-                             "║          Username       :  %-31s║\n"+
-                             "║          Email          :  %-31s║\n"+
-                             "║          Contact Number :  %-31s║\n", username,email,contactNo);
+                             "|          Username       :  %-31s|\n"+
+                             "|          Email          :  %-31s|\n"+
+                             "|          Contact Number :  %-31s|\n", username,email,contactNo);
     }
 
     public boolean checkClass(Object o){
@@ -356,9 +73,9 @@ public class User {
         }
         if (o instanceof User) {
             User user = (User) o;
-            return (username.equals(user.getAccessUsername()));
+            return (username.equals(user.getUsername()));
         }
-        return false; // the object does not belong to Event
+        return false; // the object does Not belong to Event
     }
 
     public boolean hasUser(String username){
