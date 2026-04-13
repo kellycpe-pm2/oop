ce30e047828c9945e05ed46b89b1dcacc1db46f2
Ybbtsswl <yingjieng678@gmail.com>
2026-04-13 10:55:37 +0800

    Update Ticket.java
    Signed-off-by: Ybbtsswl <yingjieng678@gmail.com>

--- Latest/Ticket.java
+++ Latest/Ticket.java
@@ -130,7 +130,7 @@ public class Ticket {
         System.out.println("Ticket ID         : " + ticketId);
         System.out.println("Booking ID        : " + bookingId);
         System.out.println("Event ID          : " + eventId);
-        System.out.println("Seat Number  : " + seatNumber);
+        System.out.println("Seat Number       : " + seatNumber);
         System.out.println("Ticket Type       :" + ticketType);
         System.out.println("Price             : " + totalAmount);
         System.out.println("Perks             : " + perks);
